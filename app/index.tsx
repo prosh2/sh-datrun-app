f0767f34ce58d1ca16b4161b613a299ae9f8950f
yzhedwin <zizo_edwin@hotmail.com>
2025-06-13 17:21:33 +0800

    [fix] Remove unnecessary alert

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,6 @@ export default function Index() {
   const { session } = useSession();
 
   if (!session) {
-    Alert.alert("You are logged out.");
     return <Redirect href="/(auth)/login" />;
   }
   return <Redirect href="/(protected)/(tabs)" />;
